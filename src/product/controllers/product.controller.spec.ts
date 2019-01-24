9733a9828b50e153a094c5180a7c2caaeb7044f4
michal <michal.pietrasz@gmail.com>
2019-01-24 20:49:01 +0100

    Add nestjs testing

--- src/product/controllers/product.controller.spec.ts
+++ src/product/controllers/product.controller.spec.ts
@@ -1,16 +1,36 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ProductController } from './product.controller';
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { ProductModule } from '../product.module';
+import { ProductService } from '../services/product.service';
+import { INestApplication } from '@nestjs/common';
+import { productFixtures } from '../fixtures/product.fixtures';
 
-describe('Product Controller', () => {
-  let module: TestingModule;
-  
+describe('Cats', () => {
+  let app: INestApplication;
+  const productService = { getAll: () => productFixtures };
+
   beforeAll(async () => {
-    module = await Test.createTestingModule({
-      controllers: [ProductController],
-    }).compile();
+    const module = await Test.createTestingModule({
+      imports: [ProductModule],
+    })
+      .overrideProvider(ProductService)
+      .useValue(productService)
+      .compile();
+
+    app = module.createNestApplication();
+    await app.init();
   });
-  it('should be defined', () => {
-    const controller: ProductController = module.get<ProductController>(ProductController);
-    expect(controller).toBeDefined();
+
+  it(`/GET product list`, () => {
+    return request(app.getHttpServer())
+      .get('/product/list')
+      .expect(200)
+      .expect(
+        productService.getAll(),
+      );
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
